3f31c86f02625932a30010738cc021c14ea2f7eb
jb <jeanb.rocher@gmail.com>
2021-10-23 10:22:27 +0200

    build(fastapi): Setup running fastapi project

---
+++ app/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from .database import engine
+from .database import SessionLocal
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
